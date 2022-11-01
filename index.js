6122ae0e6eca3f94cfdc7efb8bd35fe140ee203a
Tigran Poghosyan <tigranpoghosyan94@gmail.com>
2022-11-01 22:22:56 +0400

    Conditional statements and Data types

---
+++ index.js
@@ -0,0 +1,27 @@
+//Addition of various types
+console.log('string' + true);
+console.log('string' + 8);
+console.log(7 + false);
+
+//Multiplication of various types
+console.log('string' * true);
+console.log('string' * 8);
+console.log(7 * false);
+
+//Dividing of various types
+console.log('string' / true);
+console.log('string' / 8);
+console.log(7 / false);
+
+//Explicit conversion
+console.log( "16" / "2" );
+
+let value = true;
+value = String(value);
+console.log(typeof value);
+
+console.log(Boolean('hello'));
+console.log(Boolean(0));
+console.log(Boolean('0'));
+console.log(Boolean(''));
+
